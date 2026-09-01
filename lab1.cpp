e971536125212ec20e2caa4da48117b26e48b450
Aman Rawat <amansinghrana094@gmail.com>
2026-09-01 19:38:04 +0530

    lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,8 @@
+class Main {
+public static void main(String []args){
+  String name ="aman";
+int age = 19;
+String collage = "ABC Collage";
+System.out.println("Name: " + name);
+System.out.println("Age: " + age);
+System.out.println("Collage: " + collage); } }
