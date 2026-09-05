0181de688b10562c7b5e849c92e1852ccc72708b
Aman Rawat <amansinghrana094@gmail.com>
2026-09-05 19:16:44 +0530

    Create lab1.java
    This class displays personal information including name, age, and college.

---
+++ labsheet-01/lab1.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[]args){
+    String name = "Aman" ;
+    int age = 19 ;
+    String college = "ABC College";
+    System.out.println("Name :" + name);
+      System.out.println("Age :" + age);
+      System.out.println("College :" + college); }}
+    
+                  
