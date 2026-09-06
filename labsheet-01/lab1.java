6cebe8343857d11f6f6c506ad562bc944e0ed775
Aman Rawat <amansinghrana094@gmail.com>
2026-09-06 17:28:26 +0530

    edit

--- labsheet-01/lab1.java
+++ labsheet-01/lab1.java
@@ -5,6 +5,8 @@ class Main {
     String college = "ABC College";
     System.out.println("Name :" + name);
       System.out.println("Age :" + age);
-      System.out.println("College :" + college); }}
+      System.out.println("College :" + college); 
+  }
+}
     
                   
