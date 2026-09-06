064dbe963404b3ec86f03fef5df4787d46dd4a65
Aman Rawat <amansinghrana094@gmail.com>
2026-09-06 17:27:34 +0530

    Create lab6.java

---
+++ labsheet-01/lab6.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        int x = (int)n;
+    System.out.println("Original = " + n);
+        System.out.println("Integer = " + x);
+    }
+}
