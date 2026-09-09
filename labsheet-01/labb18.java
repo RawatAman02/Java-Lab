cd1fdadd6500f8cc80a91ee5f6bb4a4e9112e14a
Aman Rawat <amansinghrana094@gmail.com>
2026-09-09 19:12:05 +0530

    Create labb18.java

---
+++ labsheet-01/labb18.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+  public static void main(String[]args){
+    Scanner sc=new
+      Scanner(System.in);
+    int n=sc.nextInt();
+    boolean prime=n>1;
+    for(int i=2;i<=n/2;i++)
+      if(n%i==0){ prime=false;
+                 break; }
+    if(prime)
+      System.out.println("prime");
+    else System.out.println("not prime");
+  }
+}
